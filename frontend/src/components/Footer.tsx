c7f3231e2a460c33e8f906a2b376b4209c2cb794
Konstantin <altwebga@gmail.com>
2023-12-04 21:10:34 +0700

    Add: frontend structure

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+        <footer className="h-12 w-full bg-slate-200">Footer</footer>
+    )
+}
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return(
+        <header className="h-12 w-full bg-slate-200">Header</header>
+    )
+}
