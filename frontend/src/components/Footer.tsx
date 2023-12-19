f5993d10ddc26bb1ee11f0b91a92cb54f2bf5c17
Konstantin <altwebga@gmail.com>
2023-12-19 21:18:14 +0700

    new next.js

--- frontend/src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="h-12 w-full bg-slate-200">Footer</footer>
-    )
-}
--- frontend/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import NavMenu from "./NavMenu";
-
-export default function Header() {
-    return(
-       
-        <NavMenu/>
-        
-    )
-}
