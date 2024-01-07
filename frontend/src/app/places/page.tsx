560de57a32789178040fd2b2ea02181476e4581c
Konstantin <altwebga@gmail.com>
2024-01-07 17:51:48 +0700

    Add: single place

--- frontend/src/app/places/page.tsx
+++ frontend/src/app/places/page.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import PlaceCard from '@/components/PlaceCard';
-import { Place } from '@/types/places';
+import React from "react";
+import PlaceCard from "@/components/PlaceCard";
+import { Place } from "@/types/places";
 
 const Places: React.FC = async () => {
   const headers = {
-    'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
   };
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/places?populate[0]=regions&populate=feature_image`, { headers });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/places?populate[0]=regions&populate=feature_image`,
+    { headers, next: { revalidate: 3600 } }
+  );
   const data = await res.json();
   const places: Place[] = data.data;
 
   return (
-    <main className='container flex-grow m-auto'>
-      <h1>Достопримечательности</h1>
-      <div className="grid grid-cols-3 gap-4">
-      {places.map(place => (
-        <PlaceCard key={place.id} place={place} />
-      ))}
+    <main className="container flex-grow m-auto">
+      <h1 className="my-4 text-2xl">Достопримечательности</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {places.map((place) => (
+          <PlaceCard key={place.id} place={place} id={place.id}/>
+        ))}
       </div>
     </main>
   );
--- frontend/src/components/PlaceCard.tsx
+++ frontend/src/components/PlaceCard.tsx
@@ -1,28 +1,48 @@
+"use client"
+
 import React from "react";
-import { Place } from "@/types/places";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Place } from "@/types/places";
 
 interface PlaceCardProps {
+  id: number;
   place: Place;
 }
 
-const PlaceCard: React.FC<PlaceCardProps> = ({ place }) => {
+const PlaceCard: React.FC<PlaceCardProps> = ({ place, id }) => {
   const {
-    attributes: { title, feature_image },
+    attributes: { title, feature_image, regions },
   } = place;
 
+  const regionData = regions?.data?.[0]?.attributes;
+  const router = useRouter();
+
   return (
-    <div>
-        {feature_image && (
+    <button
+      type="button"
+      onClick={() => router.push(`/places/${id}`)}
+      className="max-w-sm rounded overflow-hidden shadow-lg relative"
+    >
+      {feature_image && (
+        <div className="w-full h-60 relative">
           <Image
-            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${feature_image.data.attributes.formats.thumbnail?.url}`}
-            width="300"
-            height="300"
+            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${feature_image.data.attributes.formats.medium?.url}`}
+            layout="fill"
+            objectFit="cover"
             alt={title}
           />
-        )}
-        <h2>{title}</h2>
-    </div>
+          {regionData && (
+            <p className="font-bold absolute top-0 left-0 m-4 bg-white bg-opacity-75 p-1 rounded text-sm">
+              {regionData.title}
+            </p>
+          )}
+        </div>
+      )}
+      <div className="px-6 py-4">
+        <h3 className="font-bold text-xl mb-2">{title}</h3>
+      </div>
+    </button>
   );
 };
 
