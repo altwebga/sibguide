7ec608c5493eb40319b3d1318d18d119009f2f7a
Konstantin <altwebga@gmail.com>
2024-05-23 19:48:46 +0700

    Add: popup menu

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+    <footer className="h-12 bg-slate-950 text-white">
+      <div className="">
+        footer
+      </div>
+    </footer>
+  );
+
+}
+export default Footer;
