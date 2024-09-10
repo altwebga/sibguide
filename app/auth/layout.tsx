309ab207e65e7d0ef0fd577febed6c8ad42d4bda
Konstantin <altwebga@gmail.com>
2024-09-10 21:21:55 +0700

    Add: protected route

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,16 @@
-export default function AuthLayout({
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+export default async function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      {children}
-    </div>
-  );
+  const session = await auth();
+  if (!session) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        {children}
+      </div>
+    );
+  } else {
+    redirect("/dashboard");
+  }
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,12 @@
-export default function DashboardLayout({
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth();
+  if (!session) {
+    redirect("/auth/login");
+  }
   return <>{children}</>;
 }
