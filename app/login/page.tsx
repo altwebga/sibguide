e2669a1286a7887d71d72579ed63cda445b79479
Konstantin <altwebga@gmail.com>
2024-09-20 21:53:47 +0700

    Add: private routes

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { SignInButton } from "@/components/signin-button";
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Войти</h1>
+      <SignInButton />
+    </div>
+  );
+}
